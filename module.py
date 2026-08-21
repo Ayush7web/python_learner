5fa6564fcccf398daf4531938d3a0640e1b6cc77
Ayush7web <ayushk93516@gmail.com>
2026-08-21 17:00:31 +0530

    basics rule for python apply with code

--- module.py
+++ module.py
@@ -1,4 +1,29 @@
-import pyjokes
+# import pyjokes
 
-joke = pyjokes.get_joke()
-print(joke)
+# joke = pyjokes.get_joke()
+# print(joke)
+
+name = "mockprepare"
+ 
+# print(name[-4])
+# print(name[:5])
+# print(name[0:])
+# print(name[:])
+
+# print(name[1:8:1])
+# print(name[2:10:3]) // skip of the value 
+
+# print(len(name))
+# print(name.endswith("are")) // it gives only true or false for ends string.
+
+# print(name.startswith("mock"))
+# print(name.startswith("mo")) // it gives only true or false for start string.
+
+# print(name.capitalize())
+
+
+# sentence = "my company mock prepare  will reached 400 crore  "
+# print(sentence.replace("reached", "reach"))
+
+samples = ["foods", "beverages", "cold drink", "home decour"]
+print(samples[3])
