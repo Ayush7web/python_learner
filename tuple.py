e1d6a7236c88b7d46838a27ddcc0c96d3725bfa0
Ayush7web <ayushk93516@gmail.com>
2026-08-24 11:29:29 +0530

    subsets

---
+++ tuple.py
@@ -0,0 +1,29 @@
+# a = (2,4,5,6,87)
+
+# * tuple is immutable
+# b = (3,5,7,8,2, 8364,5,6, "dkhjkjf", False)
+
+# print(type(b))
+
+# no = b.count(5)
+# no = b.index(5)
+# print(no)
+
+# e = set() //empty set//
+# s = {3,5,6,87,5,3,3} // its return only unique value.
+# print(s)
+
+
+# s = {3,4,5,65,6,4,3,3,"mock prepare"}
+# s.add(36)
+# print(s, type(set))
+
+# Sets Union and Intersection 
+
+s1 = {2,3,4,5,6,7}
+s2 = {9,9,7,4,5}
+
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print({3,87}.issubset(s1))
+# print({3,87}.issuperset(s1))
