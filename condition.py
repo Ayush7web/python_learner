15b915ed8a52357002f2725f004f399be23f113c
Ayush7web <ayushk93516@gmail.com>
2026-08-28 13:28:56 +0530

    loop & conditional statements

---
+++ condition.py
@@ -0,0 +1,37 @@
+
+# age = int(input("enter the age :"))
+
+# if(age >= 18):
+#   print("You are eligible to vote")
+
+# elif(age<0):
+#   print("Beta. tu ye kon si number daal raha hai, sahi number daal bhutni ke")
+
+
+# elif(age == 0):
+#   print("you are a zeroooo, sahi age daal")
+
+# else:
+#   print("nahi beta , tum vote nahi de sakte, jao apne ghar")
+
+# ==================================================================================
+
+
+# Loooooooooppppppp
+
+# for i in range(2,20):
+#   print(i)
+
+i = 1
+
+# while(i < 45):
+#   print(i)
+#   i +=1
+
+# for i in range(40):
+#   if(i == 20): 
+#     # break
+#     continue
+#   print(i)
+
+
